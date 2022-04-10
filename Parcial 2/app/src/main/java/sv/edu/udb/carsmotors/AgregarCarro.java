d5b2994ffa32b12d64dcc138a19d0986b90d7ee9
juan180401 <jachpera2015@gmail.com>
2022-04-09 19:54:04 -0600

    Login funcionando para administrador y cliente

---
+++ Parcial 2/app/src/main/java/sv/edu/udb/carsmotors/AgregarCarro.java
@@ -0,0 +1,14 @@
+package sv.edu.udb.carsmotors;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AgregarCarro extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_agregar_carro);
+    }
+}
--- Parcial 2/app/src/main/java/sv/edu/udb/carsmotors/Login.java
+++ Parcial 2/app/src/main/java/sv/edu/udb/carsmotors/Login.java
@@ -3,20 +3,56 @@ package sv.edu.udb.carsmotors;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import org.w3c.dom.Text;
 
 public class Login extends AppCompatActivity {
-
+    EditText usuar,passw;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        usuar=(EditText) findViewById(R.id.logusuario);
+        passw=(EditText) findViewById(R.id.logcontra);
     }
     public void autenticar (View view)
     {
-        Intent aut=new Intent(this, MainActivity.class);
-        startActivity(aut);
+        BaseDeDatos regis = new BaseDeDatos(this,"cars", null, 1);
+        SQLiteDatabase carsmotors= regis.getWritableDatabase();
+        String use1 = usuar.getText().toString();
+        String pas1 = passw.getText().toString();
+        String ti = "Administrador";
+        String ti1 = "Cliente";
+        Cursor fila = carsmotors.rawQuery("SELECT user,tipo FROM usuario WHERE password =" + pas1, null);
+        //si es administrador
+         if (fila.moveToFirst()&&use1.equals(fila.getString(0))&&ti.equals(fila.getString(1))){
+             carsmotors.close();
+             Intent aut=new Intent(this, MainActivity.class);
+             startActivity(aut);
+             usuar.setText("");
+             passw.setText("");
+
+             //si es cliente
+         } else if (fila.moveToFirst()&&use1.equals(fila.getString(0))&&ti1.equals(fila.getString(1)))
+         {
+             carsmotors.close();
+             //para mientras
+             Intent aut1=new Intent(this, Registro.class);
+             startActivity(aut1);
+             usuar.setText("");
+             passw.setText("");
+    } else
+            Toast.makeText(this, "Usuario o contraseña incorrecta",
+    Toast.LENGTH_SHORT).show();
+         usuar.setText("");
+         passw.setText("");
+        carsmotors.close();
     }
     public void registrar (View view)
     {
